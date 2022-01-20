78750f38d567af3a07003e7d782f3ce15a6afb9e
alexshtanko <alexshtanko@gmail.com>
2022-01-20 14:47:31 +0200

    Выводим кнопку "Оплатить" при формлении договора

--- wp-content/themes/intech/functions.php
+++ wp-content/themes/intech/functions.php
@@ -2333,9 +2333,35 @@ function medicalm_insurance_create_order(){
                     else
                     {
                         $result['status'] = true;
-                        $result['message'][] = '<span class="message-ok">Вітаємо, поліс успішно оформлений.</span>';
-                        $result['last_step_html'] = '<a class="get-new-medical-order" href="/medical">Оформити новий поліс</a><a target="_blank" class="download-medical-order" href="/wp-content/plugins/insurance/order-print/electronic-form/electronic-form.php?order_id=' . $order_id . '&key=kDCRa89dc0e1">Скачати поліс</a>';
+                        $result['message'][] = '<span class="message-ok">Вітаємо, данi введенi вiрно, залишилося сплатити за полiс.</span>';
+//                        $result['last_step_html'] = '<a class="get-new-medical-order" href="/medical">Оформити новий поліс</a><a target="_blank" class="download-medical-order" href="/wp-content/plugins/insurance/order-print/electronic-form/electronic-form.php?order_id=' . $order_id . '&key=kDCRa89dc0e1">Скачати поліс</a>;
+                        $result['last_step_html'] = '<form action="/wp-content/plugins/liqpay_wordpress/liqpay-form.php" method="POST">
+    <input type="hidden" name="date" value="' . date("d.m.Y H:i:s" ) . '" required/>
+    <input type="hidden" name="liqpay_product_id"  value="' . $order_id . '"/>
+    <input type="hidden" name="hidden_content"  value="test content"/>
+    <input type="hidden" name="url_page"  value="https://24.epolicy.com.ua/"/>
+    <input type="hidden" name="ip"  value="' . $_SERVER['REMOTE_ADDR'] . '"/>
+    <input type="hidden" name="pay_type"  value="pay"/>
+    <input type="hidden" name="fio" value=" . $name . "/>
+    <input type="hidden" name="mail" value=" . $email . "/>
+    <input type="hidden" id="plata" name="plata"  value="Оплата за страховой договор" />
+    <input type="hidden" id="paid" name="paid"  value="1000"/>
+    <input type="hidden" id="menu" name="menu"  value="UAH"/>
+    <input class="get-new-medical-order" type="submit" value="Оплатить" /></form>';
                         $result['order_id'] = $wpdb->insert_id;
+//                        $result['form'] = '<form action="/wp-content/plugins/liqpay_wordpress/liqpay-form.php" method="POST" style="width: 197px;" >
+//    <input type="hidden" name="date" value=' . date("d.m.Y H:i:s" ) . ' required/>
+//    <input type="hidden" name="liqpay_product_id"  value=" . $product_id . "/>
+//    <input type="hidden" name="hidden_content"  value="test content"/>
+//    <input type="hidden" name="url_page"  value="https://24.epolicy.com.ua/"/>
+//    <input type="hidden" name="ip"  value="' . $_SERVER['REMOTE_ADDR'] . '"/>
+//    <input type="hidden" name="pay_type"  value="pay"/>
+//    <input type="hidden" name="fio" value=" . $user_name . "/>
+//    <input type="hidden" name="mail" value=" . $user_email . "/>
+//    <input type="hidden" id="plata" name="plata"  value="Оплата за страховой договор" />
+//    <input type="hidden" id="paid" name="paid"  value="1000"/>
+//    <input type="hidden" id="menu" name="menu"  value="UAH"/>
+//    <input type="submit" value="Оплатить" /></form>';
                     }
 
 //                    $result['status'] = true;
@@ -2365,6 +2391,186 @@ function medicalm_insurance_create_order(){
     else{
         $result['status'] = false;
     }
+
+
+
+    /*
+     * Реализация оплаты
+     *
+     */
+//    include_once "./wp-content/plugins/liqpay_wordpress/api.php";
+//
+//
+//    $merchant_id = get_option('liqpay_merchant_id');
+//    $signature = get_option('liqpay_signature_id');
+//    $url = "https://liqpay.ua/?do=clickNbuy&button=" . $signature;
+//    $hidden_content = '';
+//
+//    $url = @(strtolower($_SERVER["HTTPS"]) != 'on') ? 'http://' . $_SERVER["SERVER_NAME"] : 'https://' . $_SERVER["SERVER_NAME"];
+//    $url .= ($_SERVER["SERVER_PORT"] != 80) ? ":" . $_SERVER["SERVER_PORT"] : "";
+//    $url .= $_SERVER["REQUEST_URI"];
+//    $url = explode('/',$url);
+//    $url1 = '';
+//    for ($i = 0; $i < count($url) - 1; $i++) {
+//        $url1 .= $url[$i] . "/";
+//    }
+//
+//    $method = '';
+//    $liqpay_phone = get_option('liqpay_phone');
+//
+//    $description = $_POST['fio'];
+//    global $user_identity,$current_user;
+//    if (wp_get_current_user()->exists()) {
+//        $current_user = wp_get_current_user();
+//    }
+//
+//    update_option('liqpay_current_user',$current_user->ID);
+//
+//    $mail = get_option('liqpay_mail_buyer');
+//    $lang = get_option('liqpay_lang');
+//    $plata = $_POST['plata'];
+//
+////    if(!isset($_POST['pay_type'])){
+////        $pay_type = "pay";
+////    }
+////    else{
+////        $pay_type = $_POST['pay_type'];
+////    }
+////    if($pay_type == 'subscribe') {
+////        $subscribe_type = $_POST['subscribe_type'];
+////    }else{
+////        $subscribe_type = '';
+////    }
+//
+//    $pay_type = 'pay';
+////    $liq_order_id = false;
+////
+////    if(isset($_POST['order_id'])) {
+////        $liq_order_id = $_POST['order_id'];
+////    }
+////    if(!$liq_order_id){
+////        $liq_order_id = rand(10000,99999);
+////    }
+//    $liq_order_id = $order_id;
+//
+//    if (isset($_POST['hidden_content'])) $hidden_content = $_POST['hidden_content'];
+//
+//    if ($hidden_content)
+//    {
+//        $result_url = $_POST['url_page'];
+//    }
+//    elseif (!get_option('liqpay_result_url') && isset($_POST['order_id']) && ($_POST['order_id'])){
+//        global $woocommerce;
+//        $order = new WC_Order($liq_order_id);
+//        $result_url = $order->get_checkout_order_received_url();
+//    }
+//    else {
+//        $result_url = get_option('liqpay_result_url');
+//    }
+//
+//    if(get_option('liqpay_thanks') == 'yes') {
+//        $server_url = $url1 . "liqpay-answer.php?server_order_id=" . $liq_order_id;
+//        $result_url = $url1 . "thank-you-page.php?server_order_id=" . $liq_order_id;
+//    }else{
+//        $server_url = $url1 . "liqpay-answer.php";
+//    }
+//
+//    $liq_order_id = $liq_order_id."_".md5(rand(0,10000));
+//    if ($_POST['mail'] !== "") update_option($liq_order_id.'-liqpay_mail_buyer',$_POST['mail']);
+//    else
+//        update_option($liq_order_id.'-liqpay_mail_buyer',$current_user->user_email);
+//
+//
+//
+//    $liq_key = false;
+//    if(isset($_POST['key']))
+//        $liq_key = $_POST['key'];
+//    update_option('liq_key',$liq_key);
+//    update_option('hidden_content',$hidden_content);
+//    $day = '';
+//    if (isset($_POST['day'])) $day = $_POST['day'];
+//    $secure_day = $day;
+//    update_option('secure_day',$secure_day);
+//    $url_page = '';
+//    if (isset($_POST['url_page'])) $url_page = $_POST['url_page'];
+//    update_option('url_page',$url_page);
+//    $ip_adress = (isset($_POST['ip']))?$_POST['ip']:'';
+//    update_option('ip_adress',$ip_adress);
+//    $skidka = vivod_skidki2();
+//    $fees_rype = get_option( 'liqpay_fees_type' );
+//    $liqpay_fees	= get_option('liqpay_fees');
+//    $liqpay_komissiya = get_option('liqpay_komissiya');
+//
+//
+//    if ($skidka > 0) {
+//        $amount = $_POST['paid'];
+//        if($liqpay_fees == 'yes') {
+//            if(!isset($_POST['order_id'])){
+//                if (($liqpay_komissiya > 0) && ($fees_rype == 'perc')) {
+//                    $amount = $amount + $amount / 100 * $liqpay_komissiya;
+//                }
+//                elseif (($liqpay_komissiya > 0) && ($fees_rype != 'perc')) {
+//                    $amount = $amount + $liqpay_komissiya;
+//                }
+//            }
+//        }
+//
+//        $amount = $amount - $amount / 100 * $skidka;
+//        $amount = round($amount,'2');
+//    }
+//    else {
+//        $amount = $_POST['paid'];
+//        if($liqpay_fees == 'yes') {
+//            if(!isset($_POST['order_id'])) {
+//                if (($liqpay_komissiya > 0) && ($fees_rype == 'perc')) {
+//                    $amount = $amount + $amount / 100 * $liqpay_komissiya;
+//                } elseif (($liqpay_komissiya > 0) && ($fees_rype != 'perc')) {
+//                    $amount = $amount + $liqpay_komissiya;
+//                }
+//            }
+//        }
+//        $amount = round($amount,'2');
+//    }
+//
+//
+//    $valuta = $_POST['menu'];
+//    $liqpay_product_id = '';
+//    if (isset($_POST['liqpay_product_id']))
+//        $liqpay_product_id = $_POST['liqpay_product_id'];
+//
+//    update_option($liq_order_id.'-liqpay_product_id',$liqpay_product_id);
+//    $plata = str_replace(',','.',$plata);
+//    $description .= "   " . $plata;
+////echo vivod_skidki2(); exit;
+//    $lqsignature = base64_encode(sha1($signature . $amount . $valuta . $merchant_id . $liq_order_id . 'buy' . $description . $result_url . $server_url,1));
+//    $testmode = get_option('liqpay_check_testmode');
+///////////////////****************************************************************** API 3.0
+//    $json_string = array('version' => '3','public_key' => $merchant_id,'amount' => $amount,'currency' => $valuta,'description' => $description,'order_id' => $liq_order_id,'action' => $pay_type, 'subscribe_periodicity'=> $subscribe_type, 'public_phone'=> $liqpay_phone,'subscribe_date_start'=> date("Y-m-d H:i:s"), 'server_url' => $server_url,'result_url' => $result_url,'pay_way' => 'card,liqpay,delayed,invoice,privat24','language' => $lang,'sandbox' => $testmode);
+////$split_rules = array ( array('public_key'=>'i4579887814','amount'=>8.5, 'commission_payer' => 'receiver','server_url' => $server_url));
+//    $data = base64_encode(json_encode($json_string));
+//
+////var_dump($server_url);
+//
+//    $liqpay = new LiqPay($merchant_id,$signature);
+//    $html = $liqpay->cnb_form(
+//        array(
+//            'version' => '3',
+//            'amount' => 1000,
+//            'currency' => 'UAH',
+//            'description' => 'Оплата за полис страхования',
+//            'order_id' => $liq_order_id,
+////        'server_url' => $server_url,
+//            'server_url' => $result_url,
+//            'result_url' => $result_url,
+//            'action' => $pay_type,
+////        'public_phone'=> $liqpay_phone,
+////        'subscribe_periodicity'=> $subscribe_type,
+////        'subscribe_date_start'=> date("Y-m-d H:i:s"),
+//            'pay_way' => 'card,liqpay,delayed,invoice,privat24',
+//            'language' => $lang,
+//            'sandbox' => $testmode//,
+//            // 'split_rules' => json_encode($split_rules)
+//        ));
 
     echo json_encode( $result );
 
